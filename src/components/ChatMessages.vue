a4fb91574d028de3cc77e9b44dae83203175fd24
Ademir Rivera <themrademir@gmail.com>
2025-01-08 14:21:35 -0600

    feat: se agrego un scroll para nuevos mensajes

--- src/components/ChatMessages.vue
+++ src/components/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div class="flex-1 overflow-y-auto p-4" ref="chat_ref">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
@@ -9,11 +9,23 @@
 
 <script setup lang="ts">
 import type { ChatMessage } from '@/interfaces/chat-message.interface'
+import { ref, watch } from 'vue'
 import ChatBubble from './ChatBubble.vue'
 
 interface Props {
   messages: ChatMessage[]
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const chat_ref = ref<HTMLDivElement | null>(null)
+
+watch(props.messages, () => {
+  setTimeout(() => {
+    chat_ref.value?.scrollTo({
+      top: chat_ref.value.scrollHeight,
+      behavior: 'smooth'
+    })
+  }, 100)
+})
 </script>
